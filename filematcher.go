92324b5360cadace7fe65aac09989a6e091067dc
Ahmed Alkabir <a.alkabir@outlook.com>
2024-01-02 00:19:09 +0200

    somefixes

--- filematcher.go
+++ filematcher.go
@@ -6,31 +6,6 @@ import (
 	filematcher "github.com/ahmedalkabir/filematcher/types"
 )
 
-// func Match(buf []byte) (FileMatcher, error) {
-// 	var file FileMatcher
-
-// 	length := len(buf)
-
-// 	if length == 0 {
-// 		return "", errors.New("EmptyBuffer")
-// 	}
-
-// 	return "Type", nil
-// }
-
-// func Match(buf []byte) (FileMatcher, error) {
-// 	var file FileMatcher
-
-// 	length := len(buf)
-
-// 	if length == 0 {
-// 		return file, errors.New("EmptyBuffer")
-// 	}
-
-// 	file.Type = DOCX_Type
-// 	return file, nil
-// }
-
 func Match(buf []byte) (filematcher.FileMatcher, error) {
 	var file filematcher.FileMatcher
 
